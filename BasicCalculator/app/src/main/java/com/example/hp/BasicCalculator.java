ee9db027a424f654410efdf61257bb996a9b70bf
aswinrakesh <aswinrakesh1999@gmail.com>
2018-02-07 19:51:47 +0530

    NEW APP

--- BasicCalculator/app/src/main/java/com/example/hp/BasicCalculator.java
+++ BasicCalculator/app/src/main/java/com/example/hp/BasicCalculator.java
@@ -1,6 +1,13 @@
 package com.example.hp;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +23,7 @@ import android.widget.Toast;
 import com.example.hp.basiccalculator.R;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static com.example.hp.basiccalculator.R.*;
 
@@ -34,10 +42,10 @@ public class BasicCalculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<String> n = new ArrayList<>();
-        n.add("+");
-        n.add("-");
-        n.add("/");
-        n.add("*");
+        n.add("ADDITION");
+        n.add("SUBTRACTION");
+        n.add("DIVISION");
+        n.add("MULTIPLICATION");
         spinner =(Spinner) findViewById(R.id.spinner);
         calc = (Button) findViewById(R.id.Calculate);
         n1 =(EditText)findViewById(R.id.editText3);
@@ -47,49 +55,75 @@ public class BasicCalculator extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
        spinner.setAdapter(adapter);
        calc.setOnClickListener(new View.OnClickListener() {
+           @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
            @SuppressLint("SetTextI18n")
            @Override
            public void onClick(View view) {
-                    String var1 = n1.getText().toString();
+                                     String var1 = n1.getText().toString();
                     String var2=n2.getText().toString();
                     int num2=Integer.parseInt(var1);
                     int num1=Integer.parseInt(var2);
                     String sp = spinner.getSelectedItem().toString();
-                        if(sp.equals("+")) {
+                    //NOTIFICATIONBUILDER
+
+               if(sp.equals("ADDITION")) {
                             sum = num1 + num2;
                             String k=String.valueOf(sum);
                             answ.setText(k);
-                        }
-                        else if(sp.equals("-")){
-                            sum=num1-num2;
-                            String l=String.valueOf(sum);
-                        answ.setText(l);}
+                            notification("sum is  "+sum);
 
-                        else if(sp.equals("*")){
+                        }
+                        else if(sp.equals("SUBTRACTION")) {
+                   sum = num1 - num2;
+                   String l = String.valueOf(sum);
+                   answ.setText(l);
+                   notification("difference is   " + sum);
+               }
+
+
+                        else if(sp.equals("MULTIPLICATION")){
                             sum=num1*num2;
                             String m=String.valueOf(sum);
-                        answ.setText(m);}
+                        answ.setText(m);
+                       notification("multiplication is   "+sum);
 
-                        else if(sp.equals("/")){
+                   }
+
+                        else if(sp.equals("DIVISION  ")){
                             sum=num1/num2;
                             String n=String.valueOf(sum);
-                        answ.setText(n);}
+                        answ.setText(n);
+                       notification("quotient is  "+sum);
+
+                   }
 
                         else {
                             answ.setText("Please select an operation");
                         }
-
-
-
-
                         }
 
+       });
 
-
-
-
-           });
     }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    void notification(String n) {
+        Random r = new Random();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel("001", "Operations", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Gives notification");
+            NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification.Builder calc = new Notification.Builder(BasicCalculator.this,"001").setSmallIcon(drawable.ic_launcher_background).setContentTitle("Calculator").setContentText(n);
+            notif.notify(r.nextInt(), calc.build());
+        } else {
+
+            NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification calc = new Notification.Builder(BasicCalculator.this).setSmallIcon(R.drawable.ic_launcher_background).setContentTitle("Calculator").setContentText(n).build();
+            notif.notify(r.nextInt(), calc);
+        }
+
+    }
+
 }
 
 
